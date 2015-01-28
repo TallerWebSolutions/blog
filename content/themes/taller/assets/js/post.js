5bb2966f94fb79c713fb112cae5cb783aeae662b
lhguerra <lhguerra@gmail.com>
2015-01-28 19:13:01 -0200

    Closes #16.

--- content/themes/taller/assets/js/post.js
+++ content/themes/taller/assets/js/post.js
@@ -8,30 +8,20 @@
 
   $(document).ready(function(){
 
-    /**
-     * Inform paragraph of image existence.
-     */
+    // Inform paragraph of image existence.
     $('.post-content img').parent('p').addClass('has-image');
 
-    /**
-     * Inform paragraph of code existence.
-     */
+    // Inform paragraph of code existence.
     $('.post-content code').parent('p').addClass('has-code');
 
-    /**
-     * Remove meta-data paragraph.
-     */
+    // Remove meta-data paragraph.
     $('.post-content meta').parent('p').hide();
 
-    /**
-     * Sticky header.
-     */
+    // Sticky header.
     (function() {
       var postMeta = $('#post-meta');
 
-      /**
-       * Apply plugin.
-       */
+      // Apply plugin.
       postMeta.stick_in_parent({
         sticky_class: 'sticked',
         brief: postMeta.outerHeight(true) * 2,
